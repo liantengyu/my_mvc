72507403007e1dbf766c94622b891278ea29ffd6
liantengyu <1263638386@qq.com>
2018-03-12 13:01:08 +0800

    new file:   config/config.php 	new file:   fastphp/Fastphp.php 	new file:   index.php 	.htaccess

---
+++ mvc/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$config = [
+	// 数据库配置
+	'db' => [
+		'host' => 'localhost',
+		'username' => 'root',
+		'password' => 'root',
+		'dbname' => 'month12',
+	],
+	// 默认控制器和操作名称
+	'defaultController' => 'Item',
+	'defaultAction' => 'index',
+
+];
+
+
+return $config;
